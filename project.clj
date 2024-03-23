6ec6496e535be0af90fd7182e1915aac4bd14321
Jeremiah Coyle <jcoyle@gmail.com>
2024-03-22 22:28:46 -0600

    Update :description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject design.kushi/domo "0.1.0"
-  :description "DOM utilities for ClojureScript"
+  :description "A ClojureScript DOM utility library"
   :url "https://github.com/kushidesign/domo"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
